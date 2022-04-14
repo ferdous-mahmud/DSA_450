b5d1933523f4b29dce2c433d478edeffbd5f4fa9
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-04-14 23:23:54 +0600

    first commit

---
+++ reverse_the_array.cpp
@@ -0,0 +1,27 @@
+// Reverse the array
+// Brout force solution
+// Time complexity: O(n)
+// Space complexity: O(n)
+#include<iostream>
+using namespace std;
+
+int main(){
+	int size = 10;
+	int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	int res[size];
+
+	int j = size;
+	for (int i = 0; i < size; ++i)
+	{
+		j --;
+		res[j] = arr[i];
+	}
+
+	for (int i = 0; i < size; ++i)
+	{
+		cout << res[i] << endl;
+	}
+
+
+    return 0;
+}
