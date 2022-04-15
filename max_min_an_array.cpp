bc5517b8b0d227b83431787555d6a827977268b2
ferdous-mahmud <ferdous.webdev@gmail.com>
2022-04-15 20:43:25 +0600

    minmax using struct added

--- max_min_an_array.cpp
+++ max_min_an_array.cpp
@@ -6,21 +6,30 @@
 #include <algorithm>
 using namespace std;
 
+struct Pair {
+	int min;
+	int max;
+};
+
 int main(){
 
 	int arr[] = {1, 3, 6, 8, 9, 0, 4, 7, 3, 7, 56};
 	int size = sizeof(arr) / sizeof(arr[0]);
 
-	int max = arr[0];
-	int min = arr[0];
+	struct Pair minmax;
+
+	minmax.min = arr[0];
+	minmax.max = arr[0];
+
 
 	for (int i = 0; i < size; ++i)
 	{
-		max = std::max(max,arr[i]);
-		min = std::min(min, arr[i]);
+		minmax.min = std::min(minmax.min,arr[i]);
+		minmax.max = std::max(minmax.max, arr[i]);
 	}
 
-	cout << min << " and " << max;
+	cout << minmax.min << endl;
+	cout << minmax.max << endl;
 
 
 	return 0;
